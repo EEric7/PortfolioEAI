79190c8676b8c2c47ca6e4aea80fdf76e26fa6ba
ELEMBA ADI Eric <30723596+EEric7@users.noreply.github.com>
2025-01-21 20:51:21 +0100

    Mise a jour des Dll

--- PortfolioEAI/obj/Debug/net8.0/PortfolioEAI.AssemblyInfo.cs
+++ PortfolioEAI/obj/Debug/net8.0/PortfolioEAI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioEAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd89ae33f02ed56d16453ee053ae43e53c3d471d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567d4368357880ecfbc112d9ad36feb36d1490d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioEAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioEAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
